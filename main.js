7447b0eee5dbdf6c067761644b718a25611c4a4f
Javier <jegj57@gmail.com>
2017-09-15 10:36:21 -0500

    Updates

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function pipeAuthentication(jwt) {
 	activateSpinner();
 	setTimeout(function() {
 		desactivateSpinner()
-		window.location.href= 'DealerAnalytics/dashboard.html';
+		window.location.href= '/DealerAnalytics/dashboard.html';
 	}, 1500);
 }
 
